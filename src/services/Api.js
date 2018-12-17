17730ad49cf36d46e3a68c3646e92072004db68c
zukethenuke <zukethenuke@hotmail.com>
2018-12-16 19:49:15 -0600

    Change heroku api to https

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default () => {
     return axios.create({
         // baseURL: `http://localhost:8081`
-        baseURL: `http://heron-haven-api.herokuapp.com`
+        baseURL: `https://heron-haven-api.herokuapp.com`
     })
 }

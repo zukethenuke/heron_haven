c4bf91a7770f1f07059d21946a49cff1d1dda9d6
zukethenuke <zukethenuke@hotmail.com>
2019-01-02 20:28:30 -0600

    Fix prod api route

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,7 +4,7 @@ import store from '@/store/store'
 export default () => {
     var env = store.state.env
     var apiURL = store.state.baseURL
-    var baseURL = env === 'dev' ? `http://${apiURL}:8081` : `https://${apiURL}`
+    var baseURL = env === 'dev' ? `http://${apiURL}:8081` : `https://heron-haven-api.herokuapp.com`
     return axios.create({
         baseURL
     })

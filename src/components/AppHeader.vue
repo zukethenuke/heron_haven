1ee9cf85d0411666eff0d999b5793028cdca0185
zukethenuke <zukethenuke@hotmail.com>
2019-01-26 18:20:50 -0600

    Update AppHeader component with icons and add links on mobile layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,43 @@
 <template>
     <span>
+<!-- ---------- -->
         <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
             <v-list>
-                <template v-for="(item, index) in DrawerItems">
-                    <v-list-tile :key="index" :to="item.route">
-                        <v-list-tile-content>
-                            {{item.title}}
-                        </v-list-tile-content>
+                <v-list-tile>
+                    <v-list-tile-action>
+                        <v-icon>home</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title>Heron Haven</v-list-tile-title>
+                </v-list-tile>
+
+                <v-list-group prepend-icon="nature_people">
+                    <v-list-tile slot="activator">
+                        <v-list-tile-title>Friends of Heron Haven</v-list-tile-title>
                     </v-list-tile>
-                    <v-divider :key="`divider-${index}`"></v-divider>
-                </template>
+                    <template v-for="(item, index) in friendsItems">
+                        <v-list-tile :key="index" :to="item.route">
+                            <v-list-tile-title>
+                                <v-icon class="title-icon-drawer">{{ item.icon }}</v-icon>
+                                {{item.title}}
+                            </v-list-tile-title>
+                        </v-list-tile>
+                        <v-divider :key="`divider-${index}`"></v-divider>
+                    </template>
+                </v-list-group>
+                <v-list-group prepend-icon="info">
+                    <v-list-tile slot="activator">
+                        <v-list-tile-title>About Us</v-list-tile-title>
+                    </v-list-tile>
+                     <v-list-tile
+                        v-for="(item, index) in aboutItems"
+                        :key="index"
+                        :to="item.route">
+                        <v-list-tile-title><v-icon class="title-icon-drawer">{{ item.icon }}</v-icon>{{item.title}}</v-list-tile-title>
+                    </v-list-tile>
+                </v-list-group>
             </v-list>
         </v-navigation-drawer>
+ <!-- ---------- -->
         <v-toolbar app color="brown darken-4" dark>
             <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
@@ -33,7 +59,7 @@
                         v-for="(item, index) in friendsItems"
                         :key="index"
                         :to="item.route">
-                        <v-list-tile-title :to="item.route">{{item.title}}</v-list-tile-title>
+                        <v-list-tile-title :to="item.route"><v-icon class="title-icon-header">{{ item.icon }}</v-icon> {{item.title}}</v-list-tile-title>
                     </v-list-tile>
                 </v-list>
             </v-menu>
@@ -49,17 +75,17 @@
                         v-for="(item, index) in aboutItems"
                         :key="index"
                         :to="item.route">
-                        <v-list-tile-title>{{item.title}}</v-list-tile-title>
+                        <v-list-tile-title><v-icon class="title-icon-header">{{ item.icon }}</v-icon>{{item.title}}</v-list-tile-title>
                     </v-list-tile>
                 </v-list>
             </v-menu>
-
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn v-if="!$store.state.isUserLoggedIn" class="hidden-sm-and-down" to="/admin">Admin</v-btn>
+            <v-btn v-if="!$store.state.isUserLoggedIn" class="hidden-sm-and-down" to="/admin"><v-icon>settings</v-icon> Admin</v-btn>
             <v-btn v-if="$store.state.isUserLoggedIn" flat class="hidden-sm-and-down" @click="signOut">Sign Out</v-btn>
             <v-btn v-if="!$store.state.isUserLoggedIn" flat class="hidden-sm-and-down" to="/signin">Sign In</v-btn>
             <!-- <v-btn v-if="!$store.state.isUserLoggedIn" color="brown lighten-3" class="hidden-sm-and-down" to="/join">Sign Up</v-btn> -->
         </v-toolbar>
+ <!-- ---------- -->
     </span>
 </template>
 
@@ -70,24 +96,17 @@ export default {
         return {
             appTitle: 'Heron Haven',
             drawer: false,
-            DrawerItems: [
-                { title: 'Home', route: '/' },
-                { title: 'Admin', route: '/admin' },
-                { title: 'Our History', route: '/history' },
-                { title: 'Sign In', route: '/signin' }
-                // { title: 'Join', route: '/join' }
-            ],
             friendsItems: [
-                { title: 'Field Trips', route: '/field_trip' },
-                { title: 'Volunteer Oppertunities', route: '/volunteer' },
-                { title: 'Our Favorite Links', route: '/links' },
-                { title: '2nd Saturday Programs', route: '/programs/2nd_saturday' },
-                { title: 'Membership Application', route: '/membership' },
-                { title: 'Contact us', route: '/contact_us' }
+                { title: 'Request Field Trip', route: '/field_trip', icon: 'school' },
+                { title: 'Volunteer Oppertunities', route: '/volunteer', icon: 'work' },
+                { title: 'Our Favorite Links', route: '/links', icon: 'favorite' },
+                { title: '2nd Saturday Programs', route: '/programs/2nd_saturday', icon: 'event' },
+                { title: 'Membership Application', route: '/membership', icon: 'card_membership' },
+                { title: 'Contact us', route: '/contact_us', icon: 'message' }
             ],
             aboutItems: [
-                { title: 'Our History', route: '/history' },
-                { title: 'Directions', route: '/map' }
+                { title: 'Our History', route: '/history', icon: 'history' },
+                { title: 'Directions', route: '/map', icon: 'directions_car' }
             ]
         }
     },
@@ -105,5 +124,12 @@ export default {
 a {
     color: white;
     text-decoration: none;
+}
+
+.title-icon-drawer {
+    margin: 0 25px;
+}
+.title-icon-header {
+    margin-right: 25px;
 }
 </style>

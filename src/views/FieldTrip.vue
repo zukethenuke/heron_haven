3e078f1ed0043f7d7e3e2f03b866f7b3786b40ae
zukethenuke <zukethenuke@hotmail.com>
2019-01-26 21:13:52 -0600

    Start work on FieldTrip component

--- src/views/FieldTrip.vue
+++ src/views/FieldTrip.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>field trips request form</div>
+    <v-container grid-list-lg fluid>
+        <v-layout row wrap>
+            <v-flex xs12 lg7 elevation-4>
+                <div>
+                    Request information about a field trip at Heron Haven
+                </div>
+            </v-flex>
+            <v-flex xs12 lg5 elevation-4>
+                Request Form
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>

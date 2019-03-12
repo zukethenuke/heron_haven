424bcfe3c9dbb1f0d497b287e68c2584b337f50e
zukethenuke <zukethenuke@hotmail.com>
2019-03-11 21:44:05 -0500

    Add pictures to About Us page

--- src/views/OurHistory.vue
+++ src/views/OurHistory.vue
@@ -1,88 +1,122 @@
 <template>
-    <div>
-        <h1>HERON HAVEN WETLAND-A WORK IN PROGRESS!</h1><br>
-        <p>The idea for saving a wetland in now the middle of the city of Omaha at 120th& Old Maple Road, came
-        about in 1991 when two neighbors, living near the wetland area made a phone call to the Audubon Society of
-        Omaha to see if the Society could help the neighbors save the wetland area from becoming a 168-apartment
-        complex. Answering the phone for the Audubon office was lone Werthman, a long time environmentalist, who
-        had worked tirelessly for years with the National Audubon Society on projects to stop the building of the Two
-        Forks Dam in Colorado, which would have destroyed the viability of the Platte River in Nebraska; to stop the
-        building the Norden Dam on the Niobrara River and in getting that river designated as a National Scenic River,
-        and in 1991when George H. W. Bush was president, she was then involved with a National Audubon nationwide grassroots project to push Congress to declare "no more net loss of wetlands in America".</p><br>
-        <p>Mrs. Werthman, visiting the Omaha site, was intrigued with seeing three Great Blue Herons leisurely
-        enjoying the waters and fish in what is now the middle of the city of Omaha, (actually then in Douglas County
-        because the area was not yet annexed to the city!) Unofficially she named the site "Heron Haven"! 75
-        neighbors going with her to the Planning Board meeting to oppose the developers plan, met with surprise
-        comments by the Omaha Planning Board Director when he said, "Omaha has no laws to protect and preserve
-        wetlands. Therefore there will be a development at this site whether it will be this developer or someone else!
-        We should just go home and live with it!"</p><br>
-        <p>Leaving the meeting, Mrs. Werthman was handed a check for $200 from one of the neighbors who said
-        "Let's buy the property from the owner", and Mrs. Werthman was asked to start fundraising! Having no idea
-        of the purchase price, Mrs. Werthman decided to approach the Papio-Missouri Natural Resource District Board
-        of Directors with a plan and to ask for help!</p><br>
-        <p>(Nebraska, as you know, is the only state in the United States that has implemented a natural resource
-        districts policy which addresses river basin management of ground and surface water, soil, conservation
-        of habitat, outdoor recreation and outdoor education.)</p><br>
-        <p>After much deliberation, the NRD Board voted to pay half of the owners price-- Werthman to secure the
-        balance! The owner was persuaded to sell to the Society and NRD instead of to the developer for the same price
-        as the developer was paying, but only ifpayment would be made within a one-month time limit! The Audubon
-        Society of Omaha voted to donate $50,000 from one of their bequests and Werthman secured donations from
-        the public for the rest of Audubon's half!</p><br>
-        <p><strong>This was the first time in the history of Nebraska that a non-profit organization cost-shared the
-        purchase of land with a Nebraska governmental agency!</strong></p><br>
-        <p>Now having 8 acres ofthe 25- acre wetland in a their possession, in 1994, the group next needed a
-        parking lot, an office and an education center in order to conduct any programs or business. Mrs. Werthman
-        then approached the owner of the "Country Bar" which was located on the premises to see ifhe would sell his
-        building and parking lot to us. He would, for $200,000 but not until 1996 after his liquor license ran out.
-        Again Mrs.Werthman, with the assistance of the Papio-Missouri River Board lawyer and a real estate salesman
-        put together a deal which the bar owner could not resist! Paying him a lower price, which we figured we could
-        afford-- two years in advance-- allowed the owner to put the money into savings to make up the difference. We
-        got the use of the parking lot and one "back" room ofthe bar with a private entrance for those two years!
-        Again, the building was purchased with a share by the Papio-Missouri River NRD, a grant from the Nebraska
-        Environmental Trust Fund, and with public donations solicited by Mrs. Werthman.
-        In 1996, after "gutting" the bar, and after much remodeling, the doors of the Heron Haven Outdoor
-        Education Center opened to the public. The center, which easily seats 75 persons, is now used by Mrs.
-        Werthman and the Heron Haven Board of Directors for nature and environmental educational programs --
-        nature movies and slide shows, nature classes for school groups, senior citizens and church groups, boy and girl
-        scouts, lunches or catered dinners.</p><br>
-        <p>A list of programs on a variety of environmental and nature subjects is enclosed and can be scheduled
-        by calling the office - 402 -493-4303. Mrs. Werthman, a former high school teacher & professional
-        photographer, has been a public speaker on environmental subjects for many years!
-        Each fall the group sponsors an open house & fall festival, bringing in live raptors, wild animals, and a
-        camaraderie of various butterflies for demonstrations and shows! A newly organized camera club is in the
-        process of photographing and recording the 141 species of birds that have already been found using the area and
-        are busy photographing as well the mink, woodchucks, deer, amphibians, butterflies, etc. which use the area.
-        Even insects are posing for the camera club members! Over 30 boy scouts have received their Eagle project
-        badges at the Haven which has helped in creating trails, fences, plantings, benches, signs, etc.
-        A butterfly garden shows off a variety of beautiful flowers all summer long, and a native prairie
-        demonstration plot shows off the assorted native grasses that grow throughout much of Nebraska.
-        In 2003, the Audubon Society of Omaha opted out of their agreement with the Papio-Missouri River
-        NRD, so, Mrs.Werthman secured a 501C3 non-profit permit from the Internal Revenue Service under the
-        name of "Friends of Heron Haven" and now holds an option lease agreement for the next 20 years with the
-        Papio-Missouri River NRD.</p><br>
-        <p>This past summer, the Friends arranged for the clean-up and digging of another section of the wetland
-        area for a new "dragonfly pond". The area was a partially sunken "concrete rubble dump site which had been
-        most likely used by the" state, county and/or city roads departments"!
-        (For the record - for years the whole 25 acres of Heron Haven had been an unofficial dump site in
-        Douglas County. Ten loads of ,junk" was hauled out of the site in 1994 with Papio-Missouri trucks when the
-        Society held several "cleanup days"! )</p><br>
-        <p>When and "if' funds become available, the dragon fly pond site will be fenced with recycled plastic
-        fencing to match the recycled plastic fence already around the butterfly garden. "Friends" received the recycled
-        plastic butterfly garden fence with a grant from the Nebraska Department of Environmental Quality (DEQ) !
-        Our enclosed latest newsletter tells of the purchase of three more acres by the Papio-Missouri river NRD
-        of the twenty-five acre site and that the Corps of Engineers will be helping this coming Fall to restore more of
-        the area!</p><br>
-        <p>Saving Heron Haven is a "work in progress"!</p><br>
-    </div>
+    <v-container fluid pa-2>
+        <v-card >
+            <h1 class="header">HERON HAVEN WETLAND-A WORK IN PROGRESS!</h1><br>
+            <v-layout pa-0 row wrap>
+                <v-flex sm12 md5 pa-2>
+                    <v-img :src="waterReflection" ></v-img>
+                </v-flex>
+                <v-flex sm12 md7 pa-2>
+                    <p>The idea for saving a wetland in now the middle of the city of Omaha at 120th& Old Maple Road, came
+                    about in 1991 when two neighbors, living near the wetland area made a phone call to the Audubon Society of
+                    Omaha to see if the Society could help the neighbors save the wetland area from becoming a 168-apartment
+                    complex. Answering the phone for the Audubon office was lone Werthman, a long time environmentalist, who
+                    had worked tirelessly for years with the National Audubon Society on projects to stop the building of the Two
+                    Forks Dam in Colorado, which would have destroyed the viability of the Platte River in Nebraska; to stop the
+                    building the Norden Dam on the Niobrara River and in getting that river designated as a National Scenic River,
+                    and in 1991when George H. W. Bush was president, she was then involved with a National Audubon nationwide grassroots project to push Congress to declare "no more net loss of wetlands in America".</p><br>
+                    <p>Mrs. Werthman, visiting the Omaha site, was intrigued with seeing three Great Blue Herons leisurely
+                    enjoying the waters and fish in what is now the middle of the city of Omaha, (actually then in Douglas County
+                    because the area was not yet annexed to the city!) Unofficially she named the site "Heron Haven"! 75
+                    neighbors going with her to the Planning Board meeting to oppose the developers plan, met with surprise
+                    comments by the Omaha Planning Board Director when he said, "Omaha has no laws to protect and preserve
+                    wetlands. Therefore there will be a development at this site whether it will be this developer or someone else!
+                    We should just go home and live with it!"</p><br>
+                    <p>Leaving the meeting, Mrs. Werthman was handed a check for $200 from one of the neighbors who said
+                    "Let's buy the property from the owner", and Mrs. Werthman was asked to start fundraising! Having no idea
+                    of the purchase price, Mrs. Werthman decided to approach the Papio-Missouri Natural Resource District Board
+                    of Directors with a plan and to ask for help!</p><br>
+                    <p>(Nebraska, as you know, is the only state in the United States that has implemented a natural resource
+                    districts policy which addresses river basin management of ground and surface water, soil, conservation
+                    of habitat, outdoor recreation and outdoor education.)</p><br>
+                </v-flex>
+            </v-layout>
+            <v-layout row wrap>
+                <v-flex sm12 md7  pa-2>
+                    <p>After much deliberation, the NRD Board voted to pay half of the owners price-- Werthman to secure the
+                    balance! The owner was persuaded to sell to the Society and NRD instead of to the developer for the same price
+                    as the developer was paying, but only ifpayment would be made within a one-month time limit! The Audubon
+                    Society of Omaha voted to donate $50,000 from one of their bequests and Werthman secured donations from
+                    the public for the rest of Audubon's half!</p><br>
+                    <p><strong>This was the first time in the history of Nebraska that a non-profit organization cost-shared the
+                    purchase of land with a Nebraska governmental agency!</strong></p><br>
+                    <p>Now having 8 acres ofthe 25- acre wetland in a their possession, in 1994, the group next needed a
+                    parking lot, an office and an education center in order to conduct any programs or business. Mrs. Werthman
+                    then approached the owner of the "Country Bar" which was located on the premises to see ifhe would sell his
+                    building and parking lot to us. He would, for $200,000 but not until 1996 after his liquor license ran out.
+                    Again Mrs.Werthman, with the assistance of the Papio-Missouri River Board lawyer and a real estate salesman
+                    put together a deal which the bar owner could not resist! Paying him a lower price, which we figured we could
+                    afford-- two years in advance-- allowed the owner to put the money into savings to make up the difference. We
+                    got the use of the parking lot and one "back" room ofthe bar with a private entrance for those two years!
+                    Again, the building was purchased with a share by the Papio-Missouri River NRD, a grant from the Nebraska
+                    Environmental Trust Fund, and with public donations solicited by Mrs. Werthman.
+                    In 1996, after "gutting" the bar, and after much remodeling, the doors of the Heron Haven Outdoor
+                    Education Center opened to the public. The center, which easily seats 75 persons, is now used by Mrs.
+                    Werthman and the Heron Haven Board of Directors for nature and environmental educational programs --
+                    nature movies and slide shows, nature classes for school groups, senior citizens and church groups, boy and girl
+                    scouts, lunches or catered dinners.</p>
+                </v-flex>
+                <v-flex sm12 md5  pa-2>
+                    <v-img :src="boardwalk"></v-img>
+                </v-flex>
+            </v-layout>
+
+            <v-layout row wrap>
+                <v-flex sm12 md5  pa-2>
+                    <v-img :src="snowyBench" ></v-img>
+                </v-flex>
+                <v-flex sm12 md7  pa-2>
+                    <p>A list of programs on a variety of environmental and nature subjects is enclosed and can be scheduled
+                    by calling the office - 402 -493-4303. Mrs. Werthman, a former high school teacher & professional
+                    photographer, has been a public speaker on environmental subjects for many years!
+                    Each fall the group sponsors an open house & fall festival, bringing in live raptors, wild animals, and a
+                    camaraderie of various butterflies for demonstrations and shows! A newly organized camera club is in the
+                    process of photographing and recording the 141 species of birds that have already been found using the area and
+                    are busy photographing as well the mink, woodchucks, deer, amphibians, butterflies, etc. which use the area.
+                    Even insects are posing for the camera club members! Over 30 boy scouts have received their Eagle project
+                    badges at the Haven which has helped in creating trails, fences, plantings, benches, signs, etc.
+                    A butterfly garden shows off a variety of beautiful flowers all summer long, and a native prairie
+                    demonstration plot shows off the assorted native grasses that grow throughout much of Nebraska.
+                    In 2003, the Audubon Society of Omaha opted out of their agreement with the Papio-Missouri River
+                    NRD, so, Mrs.Werthman secured a 501C3 non-profit permit from the Internal Revenue Service under the
+                    name of "Friends of Heron Haven" and now holds an option lease agreement for the next 20 years with the
+                    Papio-Missouri River NRD.</p><br>
+                    <p>This past summer, the Friends arranged for the clean-up and digging of another section of the wetland
+                    area for a new "dragonfly pond". The area was a partially sunken "concrete rubble dump site which had been
+                    most likely used by the" state, county and/or city roads departments"!
+                    (For the record - for years the whole 25 acres of Heron Haven had been an unofficial dump site in
+                    Douglas County. Ten loads of ,junk" was hauled out of the site in 1994 with Papio-Missouri trucks when the
+                    Society held several "cleanup days"! )</p><br>
+                    <p>When and "if' funds become available, the dragon fly pond site will be fenced with recycled plastic
+                    fencing to match the recycled plastic fence already around the butterfly garden. "Friends" received the recycled
+                    plastic butterfly garden fence with a grant from the Nebraska Department of Environmental Quality (DEQ) !
+                    Our enclosed latest newsletter tells of the purchase of three more acres by the Papio-Missouri river NRD
+                    of the twenty-five acre site and that the Corps of Engineers will be helping this coming Fall to restore more of
+                    the area!</p><br>
+                    <p>Saving Heron Haven is a "work in progress"!</p>
+                </v-flex>
+            </v-layout> 
+        </v-card>
+
+    </v-container>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            waterReflection: require('@/assets/about_us/water_reflection.jpg'),
+            boardwalk: require('@/assets/about_us/boardwalk.jpg'),
+            snowyBench: require('@/assets/about_us/snowy_bench-2.jpg'),
+
+        }
+    }
 }
 </script>
 
 <style scoped>
-    div {
-        padding: 20px 100px;
-    }
+.header {
+    text-align: center;
+    padding-top: 20px;
+}
 </style>
